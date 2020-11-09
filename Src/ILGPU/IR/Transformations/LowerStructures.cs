1b9b9bd9ee71873681bf6b12f6114fd6497ec2de
Marcel Koester <m4rs@m4rs.net>
2020-11-09 01:17:37 +0100

    Removed explicit array values from all affected transformations.

--- Src/ILGPU/IR/Transformations/LowerStructures.cs
+++ Src/ILGPU/IR/Transformations/LowerStructures.cs
@@ -573,11 +573,6 @@ namespace ILGPU.IR.Transformations
         {
             // Keep particular values that cannot be rewritten by this pass
             rewriter.Add<Parameter>((_, value) => value.Type.IsStructureType, Keep);
-            rewriter.Add<ArrayValue>((_, value) => value.Type.IsStructureType, Keep);
-            rewriter.Add<GetArrayElement>(
-                (_, value) => value.Type.IsStructureType, Keep);
-            rewriter.Add<SetArrayElement>(
-                (_, value) => value.Type.IsStructureType, Keep);
 
             // Rewrite known values
             rewriter.Add<NullValue>((_, value) => value.Type.IsStructureType, Lower);
